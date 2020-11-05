d1ab67a7bc79f1828f80672eea62681f4cf66398
nataliaortizvi <natiorvi00@gmail.com>
2020-11-05 18:48:10 -0500

    medio funciona melo

--- components/TareaList.js
+++ components/TareaList.js
@@ -15,14 +15,40 @@ class TareaList{
 
         let borrabt = document.createElement('button');
         borrabt.className = 'borrar';
+
+        let sigbt = document.createElement('button');
+        sigbt.className = 'siguiente';
+
+        let antbt = document.createElement('button');
+        antbt.className = 'anterior';
+
+        let horita = document.createElement('p');
+        horita.className = 'lahora';
+        horita.innerHTML = (
+            this.latarea.id
+        );
         
+        component.appendChild(horita);
         component.appendChild(descrip);
         component.appendChild(borrabt);
-        
+        component.appendChild(sigbt);
+        component.appendChild(antbt);
+
         borrabt.addEventListener('click', ()=>{
             const database = firebase.database();
-            database.ref('usuario/'+this.latarea.id).set(null);
+            database.ref('tareas/'+this.latarea.id).set(null);
         });
+
+        sigbt.addEventListener('click', ()=>{
+            const database = firebase.database();
+            database.ref('tareas/'+this.latarea.id+'/fase').set('tareas doing');
+        });
+
+        antbt.addEventListener('click', ()=>{
+            const database = firebase.database();
+            database.ref('tareas/'+this.latarea.id+'/fase').set('tareas to do');
+        });
+
 
         return component;
     }
--- js/index.js
+++ js/index.js
@@ -17,13 +17,14 @@ postear = () => {
     }
 
     //value de las variables
-    let referencia = database.ref('usuario/').push();   
+    let referencia = database.ref('tareas/').push();   
     let t = tareas.value;
 
     //objeto del usuario con el post
     let latarea = {
         tareasn: t,
         id: referencia.key,
+        fase: 'tareas to do',
     }  
 
     referencia.set(latarea);
@@ -34,18 +35,21 @@ postear = () => {
 btnueva.addEventListener('click',postear);
 
 //LECTURA
-database.ref('usuario/').on('value', function(data){
+database.ref('tareas/').on('value', function(data){
     todoLista.innerHTML = '';
     doingLista.innerHTML = '';
-    doneLista.innerHTML = '';
     data.forEach(
         latarea => {
             let valor = latarea.val();
-            console.log(valor);
             let fila = new TareaList(valor);
-            todoLista.appendChild(fila.render());
-            doingLista.appendChild(fila.render());
-            doneLista.appendChild(fila.render());
+            if(valor.fase == 'tareas to do'){
+                todoLista.appendChild(fila.render());
+            }
+        
+            if(valor.fase == 'tareas doing'){
+                doingLista.appendChild(fila.render());
+            }
+            
         }
     );
 });
